6dc334cb532a71f99e157f8d88afcf983dac868c
EC2 Default User <ec2-user@ip-172-16-56-69.ec2.internal>
2024-06-13 02:56:28 +0000

    change icdar2013 to bsmock/ICDAR-2013.c

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,11 +1,17 @@
 # sudo apt update
 # sudo apt install git-lfs -y
 git lfs install
+
 git clone https://huggingface.co/datasets/bsmock/FinTabNet.c
 cd FinTabNet.c && bash extract_fintabnet.sh
+
 git clone https://huggingface.co/datasets/bsmock/pubtables-1m
 cd pubtables-1m && bash extract_structure_dataset.sh
-git clone https://huggingface.co/datasets/bsmock/ICDAR-2013-Table-Competition-Corrected
-cd ICDAR-2013-Table-Competition-Corrected && tar xvf ICDAR-2013-Table-Competition-Corrected.tar.gz
-pip install editdistance
-python process_icdar2013.py --data_dir ICDAR-2013-Table-Competition-Corrected/ICDAR-2013-Table-Competition-Corrected --output_dir ICDAR-2013.c
+
+# git clone https://huggingface.co/datasets/bsmock/ICDAR-2013-Table-Competition-Corrected
+# cd ICDAR-2013-Table-Competition-Corrected && tar xvf ICDAR-2013-Table-Competition-Corrected.tar.gz
+# pip install editdistance
+# python process_icdar2013.py --data_dir ICDAR-2013-Table-Competition-Corrected/ICDAR-2013-Table-Competition-Corrected --output_dir ICDAR-2013.c
+
+git clone https://huggingface.co/datasets/bsmock/ICDAR-2013.c
+cd ICDAR-2013.c && bash extract_icdar2013.sh
