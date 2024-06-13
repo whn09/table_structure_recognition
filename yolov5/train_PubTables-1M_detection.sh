265142ceeba15526eca84ed250433da60c148fec
EC2 Default User <ec2-user@ip-172-16-56-69.ec2.internal>
2024-06-13 03:36:54 +0000

    add detection-800 val

--- yolov5/train_PubTables-1M_detection.sh
+++ yolov5/train_PubTables-1M_detection.sh
@@ -1,4 +1,7 @@
-python train.py --workers 8 --device 0 --batch-size 64 --data data/custom-detection.yaml --img 800 --weights yolov5s.pt --name yolov5s-custom-detection --epochs 10
-# python train.py --workers 8 --device 0 --batch-size 2 --data data/custom-detection.yaml --img 1280 --weights yolov5x6.pt --name yolov5x6-custom-detection --epochs 10
+# python train.py --workers 8 --device 0 --batch-size 64 --data data/custom-detection.yaml --img 640 --weights yolov5s.pt --name yolov5s-custom-detection --epochs 10
 
-python val.py --workers 8 --device 0 --batch-size 128 --data data/custom-detection.yaml --img 800 --task test --weights runs/train/yolov5s-custom-detection/weights/best.pt --name yolov5s-custom-detection
+# python val.py --workers 8 --device 0 --batch-size 128 --data data/custom-detection.yaml --img 640 --task test --weights runs/train/yolov5s-custom-detection/weights/best.pt --name yolov5s-custom-detection
+
+python train.py --workers 8 --device 0 --batch-size 64 --data data/custom-detection.yaml --img 800 --weights yolov5s.pt --name yolov5s-custom-detection-800 --epochs 10
+
+python val.py --workers 8 --device 0 --batch-size 128 --data data/custom-detection.yaml --img 800 --task test --weights runs/train/yolov5s-custom-detection-800/weights/best.pt --name yolov5s-custom-detection-800
--- yolov5/train_PubTables-1M_structure.sh
+++ yolov5/train_PubTables-1M_structure.sh
@@ -1,4 +1,3 @@
-python train.py --workers 8 --device 0 --batch-size 64 --data data/custom-structure.yaml --img 1000 --weights yolov5s.pt --name yolov5s-custom-structure --epochs 10
-# python train.py --workers 8 --device 0 --batch-size 2 --data data/custom-structure.yaml --img 1280 --weights yolov5x6.pt --name yolov5x6-custom --epochs 10
+python train.py --workers 8 --device 0 --batch-size 64 --data data/custom-structure.yaml --img 640 --weights yolov5s.pt --name yolov5s-custom-structure --epochs 10
 
-python val.py --workers 8 --device 0 --batch-size 128 --data data/custom-structure.yaml --img 1000 --task test --weights runs/train/yolov5s-custom-structure/weights/best.pt --name yolov5s-custom-structure
+python val.py --workers 8 --device 0 --batch-size 128 --data data/custom-structure.yaml --img 640 --task test --weights runs/train/yolov5s-custom-structure/weights/best.pt --name yolov5s-custom-structure
