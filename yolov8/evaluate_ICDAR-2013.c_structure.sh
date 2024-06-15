9f049eba37a5e0acebc4f840a82442495c1e5250
Ubuntu <ubuntu@ip-172-31-48-146.us-west-2.compute.internal>
2024-06-15 11:07:50 +0000

    add yolov8x

--- yolov8/evaluate_ICDAR-2013.c_structure.sh
+++ yolov8/evaluate_ICDAR-2013.c_structure.sh
@@ -2,3 +2,4 @@ pip install ultralytics
 yolo detect val split=test data=data/custom-structure-icdar2013.yaml model=runs/detect/yolov8s-custom-structure/weights/best.pt imgsz=1024 device=0 workers=8 batch=16
 yolo detect val split=test data=data/custom-structure-icdar2013.yaml model=runs/detect/yolov8s-custom-structure-fintabnet/weights/best.pt imgsz=1024 device=0 workers=8 batch=16
 yolo detect val split=test data=data/custom-structure-icdar2013.yaml model=runs/detect/yolov8s-custom-structure-all/weights/best.pt imgsz=1024 device=0 workers=8 batch=16
+yolo detect val split=test data=data/custom-structure-icdar2013.yaml model=runs/detect/yolov8x-custom-structure-all/weights/best.pt imgsz=1024 device=0 workers=8 batch=16
--- yolov8/train_all_structure.sh
+++ yolov8/train_all_structure.sh
@@ -1,3 +1,5 @@
 pip install ultralytics
 yolo detect train data=data/custom-structure-all.yaml model=yolov8s.pt name=yolov8s-custom-structure-all imgsz=1024 epochs=10 device=0,1,2,3,4,5,6,7 workers=96 batch=768
 yolo detect val split=test data=data/custom-structure-all.yaml model=runs/detect/yolov8s-custom-structure-all/weights/best.pt imgsz=1024 device=0 workers=96 batch=512
+
+# yolo detect train data=data/custom-structure-all.yaml model=yolov8x.pt name=yolov8x-custom-structure-all imgsz=1024 epochs=10 device=0,1,2,3,4,5,6,7 workers=32 batch=128
